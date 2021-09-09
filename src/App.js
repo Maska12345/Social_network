7888764e9e6e27acaadb11c2cdbaffe2840bbc0f
Max <maxgerun@gmail.com>
2021-09-09 17:29:32 +0300

    added images

--- src/App.js
+++ src/App.js
@@ -3,25 +3,29 @@ import './App.css';
 
 const App = () => {
   return (
-    <div className="App">
+    <div className="wrapper">
+
     <header className='header'>
-        <div className='logo'>
-            <img src="https://img.pngio.com/atom-icon-png-401284-free-icons-library-atomic-png-256_256.jpg"/>
-        </div>
+            <img className='logo' src="https://img.pngio.com/atom-icon-png-401284-free-icons-library-atomic-png-256_256.jpg"/>
     </header>
         <nav className='nav'>
-            <div>
                 <div>
-                    Profile
+                    <a href="#">Profile</a>
                 </div>
                 <div>
-                    Messages
+                    <a href="#">Messagese</a>
                 </div>
                 <div>
-                    Settings
+                    <a href="#">News</a>
                 </div>
-            </div>
+                <div>
+                    <a href="#">Settings</a>
+                </div>
         </nav>
+        <div className='content'>
+            <img src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"/>
+        </div>
+
     </div>
   );
 }
