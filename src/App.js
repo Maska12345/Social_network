dd87d0bfdddc8000ba596e66b8bd404d6476d1f0
Max <maxgerun@gmail.com>
2021-09-09 16:50:01 +0300

    added logo

--- src/App.js
+++ src/App.js
@@ -4,7 +4,24 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
-    test
+    <header className='header'>
+        <div className='logo'>
+            <img src="https://img.pngio.com/atom-icon-png-401284-free-icons-library-atomic-png-256_256.jpg"/>
+        </div>
+    </header>
+        <nav className='nav'>
+            <div>
+                <div>
+                    Profile
+                </div>
+                <div>
+                    Messages
+                </div>
+                <div>
+                    Settings
+                </div>
+            </div>
+        </nav>
     </div>
   );
 }
