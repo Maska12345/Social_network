52eeacc097114a168763e82d269267992ea35e5b
Maska12345 <maxgerun@gmail.com>
2021-09-14 23:14:24 +0300

    added dialogs

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import './App.css';
 import Header from "./Components/Header/Header";
 import Nav from "./Components/Nav/Nav";
 import Profile from "./Components/Profile/Profile";
+import Dialogs from "./Components/Dialogs/Dialogs";
 
 const App = () => {
   return (
     <div className="wrapper">
         <Header />
         <Nav />
-        <Profile />
+        <div className='appContent'>
+            <Profile />
+        </div>
+        <Dialogs />
+
     </div>
   );
 }
---
+++ src/Components/Dialogs/Dialogs.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import DialogsStyle from './Dialogs.module.css'
+
+const Dialogs = () =>{
+    return(
+        <div>Dialogs</div>
+    )
+}
+
+export default Dialogs;
