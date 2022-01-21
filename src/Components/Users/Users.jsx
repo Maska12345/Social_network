f2d045d5a576bba5ebae4856d932b381431f9625
Maska12345 <maxgerun@gmail.com>
2022-01-22 00:24:38 +0200

    get users from API

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -1,29 +1,44 @@
 import React from "react";
 import styles from './users.module.css';
+import * as axios from "axios";
+import UserPhoto from '../../assets/images/user_default.png'
 
-const Users = (props) =>{
-    return(
+const Users = (props) => {
+    let getUsers = () =>{
+        if (props.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+                props.setUsers(response.data.items);
+            })
+    }
+
+    }
+
+    return (
+
         <div>
+            <div onClick={getUsers}><button>Get Users</button></div>
             {props.users.map(u =>
-            <div key={u.id}>
+                <div key={u.id}>
                 <span>
-                    <div><img src={u.photoUrl} className={styles.userPhoto}/></div>
+                    <div><img src={u.photos.small != null ? u.photos.small : UserPhoto}
+                              className={styles.userPhoto}/></div>
                     <div>
-                        {u.followed?<button onClick={()=>props.unfollow(u.id)}>UnFollow</button>:<button onClick={()=>props.follow(u.id)}>Follow</button>}
+                        {u.followed ? <button onClick={() => props.unfollow(u.id)}>UnFollow</button> :
+                            <button onClick={() => props.follow(u.id)}>Follow</button>}
                     </div>
                 </span>
-                <span>
                     <span>
-                        <div>{u.fullName}</div>
+                    <span>
+                        <div>{u.name}</div>
                         <div>{u.status}</div>
                     </span>
                     <span>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
+                        <div>{"u.location.country"}</div>
+                        <div>{"u.location.city"}</div>
                     </span>
                 </span>
 
-            </div>)}
+                </div>)}
         </div>
     )
 
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -3,14 +3,14 @@ const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
 
 let initinalState = {
-    users: [
-        {id: 1,photoUrl:'https://rusradio.lt/wp-content/uploads/2021/12/maxkorzh-logo.jpg',followed:false, fullName: 'Max', status: 'I am react developer',location:{country:'Ukraine',city:'Vinnytsia'}},
-        {id: 2,photoUrl:'https://upload.wikimedia.org/wikipedia/commons/c/cf/%D0%94%D0%B5%D0%BD%D0%B8%D1%81_%D0%A1%D0%B5%D0%BC%D0%B5%D0%BD%D0%B8%D1%85%D0%B8%D0%BD_%28cropped%29.png',followed:true, fullName: 'Denis', status: 'I like big dogs',location:{country:'Belarus',city:'Minsk'}},
-        {id: 3,photoUrl:'https://odnaminyta.com/wp-content/uploads/2020/12/05b6de907f0014d85d1af221ec0c3078.jpg',followed:true, fullName: 'Stepan', status: 'Open to work',location:{country:'Ukraine',city:'Kyiv'}},
-        {id: 4,photoUrl:'https://hostelcity.su/wp-content/uploads/po-vostochnomu-kalendaryu-aleksandr-medvedev-vladimirovich-krolik.jpg',followed:false, fullName: 'Shura', status: 'I find new friends',location:{country:'Poland',city:'Krakow'}},
-        {id: 5,photoUrl:'https://sobesednik.ru/sites/default/files/complex_images/images/gena_bukin.jpg',followed:true, fullName: 'Gena', status: 'I can help with Android App',location:{country:'USA',city:'Los-Angeles'}},
-
-    ]
+     users: [
+    //     {id: 1,photoUrl:'https://rusradio.lt/wp-content/uploads/2021/12/maxkorzh-logo.jpg',followed:false, fullName: 'Max', status: 'I am react developer',location:{country:'Ukraine',city:'Vinnytsia'}},
+    //     {id: 2,photoUrl:'https://upload.wikimedia.org/wikipedia/commons/c/cf/%D0%94%D0%B5%D0%BD%D0%B8%D1%81_%D0%A1%D0%B5%D0%BC%D0%B5%D0%BD%D0%B8%D1%85%D0%B8%D0%BD_%28cropped%29.png',followed:true, fullName: 'Denis', status: 'I like big dogs',location:{country:'Belarus',city:'Minsk'}},
+    //     {id: 3,photoUrl:'https://odnaminyta.com/wp-content/uploads/2020/12/05b6de907f0014d85d1af221ec0c3078.jpg',followed:true, fullName: 'Stepan', status: 'Open to work',location:{country:'Ukraine',city:'Kyiv'}},
+    //     {id: 4,photoUrl:'https://hostelcity.su/wp-content/uploads/po-vostochnomu-kalendaryu-aleksandr-medvedev-vladimirovich-krolik.jpg',followed:false, fullName: 'Shura', status: 'I find new friends',location:{country:'Poland',city:'Krakow'}},
+    //     {id: 5,photoUrl:'https://sobesednik.ru/sites/default/files/complex_images/images/gena_bukin.jpg',followed:true, fullName: 'Gena', status: 'I can help with Android App',location:{country:'USA',city:'Los-Angeles'}},
+    //
+     ]
 }
 
 const usersReducer = (state = initinalState, action) => {
